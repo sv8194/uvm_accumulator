8ec161920c35c53529f1778c40fc0a5669fdf106
yibing <sv8194@gmail.com>
2022-11-09 10:22:24 -0800

    assertion added to check accumulator values

--- UVM_Tests/sva_top.sv
+++ UVM_Tests/sva_top.sv
@@ -10,12 +10,25 @@ module sva_top #(
 	input logic        						clear_i,		// clear
 	input logic signed [DIN_WIDTH-1:0]		data_i,			// input data
 
-	input logic signed [DOUT_WIDTH-1:0]	result_o
+	input logic signed [DOUT_WIDTH-1:0]		result_o
 );
+
+	logic signed [DIN_WIDTH-1:0]	data_p;
+	logic signed [DOUT_WIDTH-1:0]	result_p;
+
+	always@(posedge clk) begin
+		data_p <= #2 data_i;
+		result_p <= #2 result_o;
+	end
 
 	property pp0;
 		@(posedge clk) clear_i |-> ##1 (result_o == 'd0);
 	endproperty
 	assert property(pp0);
+
+	property pp1;
+		@(posedge clk) en_i |-> ##1 (result_o == (data_p + result_p));
+	endproperty
+	assert property(pp1);
 
 endmodule
